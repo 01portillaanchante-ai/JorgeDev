f95f764547721754d02dc0c93910b5c93a3f8fa3
01portillaanchante-ai <01portillaanchante@gmail.com>
2026-04-03 21:28:47 -0300

    Add files via upload
    Mi primer programa corto

---
+++ Mi proyecto weeb/Hola.py
@@ -0,0 +1,4 @@
+nombre = input("escriba su nombre")
+apellido = input("escriba su apellido") 
+
+print("Hola cliente como estas como se llama:", nombre, "y su apellido:", apellido, "muchas gracias en un momento le damos su respuesta")
